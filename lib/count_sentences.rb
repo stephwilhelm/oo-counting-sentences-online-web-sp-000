4d8d05a502f0249eb5f5dc1c594d177a9f4b5b13
Steph W <wilhelm.steph@gmail.com>
2020-05-24 19:50:57 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,20 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    if self.end_with?(".") = true
-    else = false
-    end
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
   end
 end
